cebc450c01905e0c3c009d3b8a3a5e57a7825479
aim.fathi@gmail.com <aim.fathi@gmail.com>
2022-08-11 14:30:03 +0200

    replacing email by username : username constraint, modifying forms rendering

--- src/Form/PassResetRequestType.php
+++ src/Form/PassResetRequestType.php
@@ -12,7 +12,7 @@ class PassResetRequestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('username')
         ;
     }
 
